0c26c0e67110c4d866478b2872594df1b10f5dfe
Roman Zipp <ich@ich.wtf>
2020-03-27 12:05:53 +0100

    Update testcase disaptching jobs method

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,12 +2,11 @@
 
 namespace romanzipp\QueueMonitor\Tests;
 
-use Illuminate\Contracts\Bus\Dispatcher;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Queue\QueueManager;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 use romanzipp\QueueMonitor\Providers\QueueMonitorProvider;
-use romanzipp\QueueMonitor\Tests\Support\Job;
 
 class TestCase extends BaseTestCase
 {
@@ -15,9 +14,10 @@ class TestCase extends BaseTestCase
 
     protected function dispatch(ShouldQueue $job)
     {
-        app(Dispatcher::class)->dispatch(
-            $job
-        );
+        app(QueueManager::class)->push($job);
+
+        // Not attaching Job
+        // app(Dispatcher::class)->dispatch($job);
     }
 
     protected function getPackageProviders($app)
