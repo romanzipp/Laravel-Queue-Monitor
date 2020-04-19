595e47410ca41ad5c6d1507810a7d87d0376fce5
Roman Zipp <ich@ich.wtf>
2020-04-19 22:33:36 +0200

    Update ui view

--- views/jobs.blade.php
+++ views/jobs.blade.php
@@ -2,7 +2,12 @@
 <html lang="en">
 
 <head>
-    <title>Jobs</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Queue Monitor</title>
+
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 
 </head>
