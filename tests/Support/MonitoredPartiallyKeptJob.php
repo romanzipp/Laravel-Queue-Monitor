e25d17b713dc00fd02ae0b9c0d3bee1e286ebe5f
Roman Zipp <ich@ich.wtf>
2020-05-02 22:10:58 +0200

    Add static method to delete successful monitors on completion

---
+++ tests/Support/MonitoredPartiallyKeptJob.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace romanzipp\QueueMonitor\Tests\Support;
+
+use romanzipp\QueueMonitor\Traits\IsMonitored;
+
+class MonitoredPartiallyKeptJob extends BaseJob
+{
+    use IsMonitored;
+
+    public static function keepMonitorOnSuccess(): bool
+    {
+        return false;
+    }
+}
