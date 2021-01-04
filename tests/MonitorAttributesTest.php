ec8cf9acd56c597f220a794fa0b17e809ab35eec
Roman Zipp <ich@ich.wtf>
2021-01-04 14:28:06 +0100

    Add more cooldown tests

--- tests/MonitorAttributesTest.php
+++ tests/MonitorAttributesTest.php
@@ -9,6 +9,7 @@ use romanzipp\QueueMonitor\Tests\Support\MonitoredJobWithMergedData;
 use romanzipp\QueueMonitor\Tests\Support\MonitoredJobWithMergedDataConflicting;
 use romanzipp\QueueMonitor\Tests\Support\MonitoredJobWithProgress;
 use romanzipp\QueueMonitor\Tests\Support\MonitoredJobWithProgressCooldown;
+use romanzipp\QueueMonitor\Tests\Support\MonitoredJobWithProgressCooldownMockingTime;
 
 class MonitorAttributesTest extends TestCase
 {
@@ -103,5 +104,35 @@ class MonitorAttributesTest extends TestCase
         $this->assertInstanceOf(Monitor::class, $monitor = Monitor::query()->first());
         $this->assertEquals(MonitoredJobWithProgressCooldown::class, $monitor->name);
         $this->assertEquals(0, $monitor->progress);
+    }
+
+    public function testProgressStandbyInFuture()
+    {
+        $this->dispatch(new MonitoredJobWithProgressCooldownMockingTime(0));
+        $this->workQueue();
+
+        $this->assertInstanceOf(Monitor::class, $monitor = Monitor::query()->first());
+        $this->assertEquals(MonitoredJobWithProgressCooldownMockingTime::class, $monitor->name);
+        $this->assertEquals(0, $monitor->progress);
+    }
+
+    public function testProgressStandbyInFutureIgnoredValue()
+    {
+        $this->dispatch(new MonitoredJobWithProgressCooldownMockingTime(50));
+        $this->workQueue();
+
+        $this->assertInstanceOf(Monitor::class, $monitor = Monitor::query()->first());
+        $this->assertEquals(MonitoredJobWithProgressCooldownMockingTime::class, $monitor->name);
+        $this->assertEquals(50, $monitor->progress);
+    }
+
+    public function testProgressStandbyInFutureTen()
+    {
+        $this->dispatch(new MonitoredJobWithProgressCooldownMockingTime(10));
+        $this->workQueue();
+
+        $this->assertInstanceOf(Monitor::class, $monitor = Monitor::query()->first());
+        $this->assertEquals(MonitoredJobWithProgressCooldownMockingTime::class, $monitor->name);
+        $this->assertEquals(10, $monitor->progress);
     }
 }
