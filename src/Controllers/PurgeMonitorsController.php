790ee2b41fe56bdba6917b98b04ed13d9e5d8b9e
Roman Zipp <ich@ich.wtf>
2021-02-12 13:47:04 +0100

    Add http endpoints to delete & purge entries

---
+++ src/Controllers/PurgeMonitorsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace romanzipp\QueueMonitor\Controllers;
+
+use Illuminate\Http\Request;
+use romanzipp\QueueMonitor\Models\Contracts\MonitorContract;
+use romanzipp\QueueMonitor\Services\QueueMonitor;
+
+class PurgeMonitorsController
+{
+    public function __invoke(Request $request)
+    {
+        $model = QueueMonitor::getModel();
+
+        $model->newQuery()->each(function (MonitorContract $monitor) {
+            $monitor->delete();
+        }, 200);
+
+        return redirect()->action(ShowQueueMonitorController::class);
+    }
+}
