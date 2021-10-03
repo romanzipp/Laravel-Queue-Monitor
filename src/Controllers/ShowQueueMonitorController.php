dedcd6fbfd2806db397536dbae8e9144a04b36bc
romanzipp <romanzipp@users.noreply.github.com>
2021-10-03 11:23:51 +0000

    Fix styling

--- src/Controllers/ShowQueueMonitorController.php
+++ src/Controllers/ShowQueueMonitorController.php
@@ -16,6 +16,7 @@ class ShowQueueMonitorController
 {
     /**
      * @param \Illuminate\Http\Request $request
+     *
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function __invoke(Request $request)
