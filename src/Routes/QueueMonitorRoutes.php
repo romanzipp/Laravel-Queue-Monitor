cf6f2ad12856bd11766aa69d86a42458a04350db
Roman Zipp <ich@ich.wtf>
2022-11-22 12:09:24 +0100

    Update routing

--- src/Routes/QueueMonitorRoutes.php
+++ src/Routes/QueueMonitorRoutes.php
@@ -2,8 +2,6 @@
 
 namespace romanzipp\QueueMonitor\Routes;
 
-use romanzipp\QueueMonitor\Controllers;
-
 class QueueMonitorRoutes
 {
     /**
@@ -15,14 +13,14 @@ class QueueMonitorRoutes
     {
         return function (array $options = []) {
             /** @var \Illuminate\Routing\Router $this */
-            $this->get('', Controllers\ShowQueueMonitorController::class)->name('queue-monitor::index');
+            $this->get('', '\romanzipp\QueueMonitor\Controllers\ShowQueueMonitorController')->name('queue-monitor::index');
 
             if (config('queue-monitor.ui.allow_deletion')) {
-                $this->delete('monitors/{monitor}', Controllers\DeleteMonitorController::class)->name('queue-monitor::destroy');
+                $this->delete('monitors/{monitor}', '\romanzipp\QueueMonitor\Controllers\DeleteMonitorController')->name('queue-monitor::destroy');
             }
 
             if (config('queue-monitor.ui.allow_purge')) {
-                $this->delete('purge', Controllers\PurgeMonitorsController::class)->name('queue-monitor::purge');
+                $this->delete('purge', '\romanzipp\QueueMonitor\Controllers\PurgeMonitorsController')->name('queue-monitor::purge');
             }
         };
     }
