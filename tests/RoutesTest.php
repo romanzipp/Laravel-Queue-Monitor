cfd9f2bf6bb2d3a4b6dd1a65776419036f809810
Roman Zipp <ich@ich.wtf>
2023-05-11 16:25:10 +0200

    Add routes test

---
+++ tests/RoutesTest.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace romanzipp\QueueMonitor\Tests;
+
+use romanzipp\QueueMonitor\Models\Monitor;
+use romanzipp\QueueMonitor\Tests\TestCases\DatabaseTestCase;
+
+class RoutesTest extends DatabaseTestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withExceptionHandling();
+    }
+
+    /*
+     *--------------------------------------------------------------------------
+     * Index
+     *--------------------------------------------------------------------------
+     */
+
+    public function testIndexDisabled()
+    {
+        config(['queue-monitor.ui.enabled' => false]);
+
+        $this
+            ->get('/jobs')
+            ->assertStatus(404);
+    }
+
+    public function testIndexEnabled()
+    {
+        config(['queue-monitor.ui.enabled' => true]);
+
+        $this
+            ->get('/jobs')
+            ->assertStatus(200)
+            ->assertViewIs('queue-monitor::jobs');
+    }
+
+    /*
+     *--------------------------------------------------------------------------
+     * Delete Monitor
+     *--------------------------------------------------------------------------
+     */
+
+    public function testDeleteDisabledUi()
+    {
+        config(['queue-monitor.ui.enabled' => false]);
+
+        /** @var \romanzipp\QueueMonitor\Models\Monitor $monitor */
+        $monitor = Monitor::query()->create([
+            'job_id' => mt_rand(),
+        ]);
+
+        $this
+            ->delete("/jobs/{$monitor->id}")
+            ->assertStatus(404);
+    }
+
+    public function testDeleteDisabledDeletion()
+    {
+        config([
+            'queue-monitor.ui.enabled' => true,
+            'queue-monitor.ui.allow_deletion' => false,
+        ]);
+
+        /** @var \romanzipp\QueueMonitor\Models\Monitor $monitor */
+        $monitor = Monitor::query()->create([
+            'job_id' => mt_rand(),
+        ]);
+
+        $this
+            ->delete("/jobs/monitors/{$monitor->id}")
+            ->assertStatus(404);
+    }
+
+    public function testDeleteEnabled()
+    {
+        config([
+            'queue-monitor.ui.enabled' => true,
+            'queue-monitor.ui.allow_deletion' => true,
+        ]);
+
+        /** @var \romanzipp\QueueMonitor\Models\Monitor $monitor */
+        $monitor = Monitor::query()->create([
+            'job_id' => mt_rand(),
+        ]);
+
+        $this
+            ->delete("/jobs/monitors/{$monitor->id}")
+            ->assertStatus(302)
+            ->assertRedirectToRoute('queue-monitor::index');
+    }
+
+    /*
+     *--------------------------------------------------------------------------
+     * Purge
+     *--------------------------------------------------------------------------
+     */
+
+    public function testPurgeDisabledUi()
+    {
+        config(['queue-monitor.ui.enabled' => false]);
+
+        $this
+            ->delete('/jobs/purge')
+            ->assertStatus(404);
+    }
+
+    public function testPurgeDisabledPurging()
+    {
+        config([
+            'queue-monitor.ui.enabled' => true,
+            'queue-monitor.ui.allow_purge' => false,
+        ]);
+
+        $this
+            ->delete('/jobs/purge')
+            ->assertStatus(404);
+    }
+
+    public function testPurgeEnabled()
+    {
+        config([
+            'queue-monitor.ui.enabled' => true,
+            'queue-monitor.ui.allow_purge' => true,
+        ]);
+
+        $this
+            ->delete('/jobs/purge')
+            ->assertStatus(302)
+            ->assertRedirectToRoute('queue-monitor::index');
+    }
+}
