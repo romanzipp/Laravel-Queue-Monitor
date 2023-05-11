0a16bbdf2f6145ef9d529f2106484147d748ad48
Roman Zipp <ich@ich.wtf>
2023-05-11 16:23:28 +0200

    Add other routes to enabled check in middleware

--- src/Middleware/CheckQueueMonitorUiConfig.php
+++ src/Middleware/CheckQueueMonitorUiConfig.php
@@ -3,13 +3,26 @@
 namespace romanzipp\QueueMonitor\Middleware;
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Route;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CheckQueueMonitorUiConfig
 {
     public function handle(Request $request, \Closure $next)
     {
-        if ( ! config('queue-monitor.ui.enabled')) {
+        $route = $request->route();
+
+        if ( ! ($route instanceof Route)) {
+            throw new NotFoundHttpException('Not Found');
+        }
+
+        $allowed = match ($route->getName()) {
+            'queue-monitor::index' => config('queue-monitor.ui.enabled'),
+            'queue-monitor::destroy' => config('queue-monitor.ui.enabled') && config('queue-monitor.ui.allow_deletion'),
+            'queue-monitor::purge' => config('queue-monitor.ui.enabled') && config('queue-monitor.ui.allow_purge'),
+        };
+
+        if ( ! $allowed) {
             throw new NotFoundHttpException('Not Found');
         }
 
