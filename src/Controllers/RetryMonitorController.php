95bd77e9f513ec6121c90a402bfd465ad1e4f32f
Roman Zipp <ich@ich.wtf>
2023-09-04 12:21:12 +0200

    Use configured model class on retry endpoint

--- src/Controllers/RetryMonitorController.php
+++ src/Controllers/RetryMonitorController.php
@@ -7,13 +7,15 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use romanzipp\QueueMonitor\Enums\MonitorStatus;
 use romanzipp\QueueMonitor\Models\Monitor;
+use romanzipp\QueueMonitor\Services\QueueMonitor;
 
 class RetryMonitorController
 {
     public function __invoke(Request $request, int $monitorId): RedirectResponse
     {
         /** @var \romanzipp\QueueMonitor\Models\Monitor $monitor */
-        $monitor = Monitor::query()
+        $monitor = QueueMonitor::getModel()
+            ->query()
             ->where('status', MonitorStatus::FAILED)
             ->where('retried', false)
             ->whereNotNull('job_uuid')
