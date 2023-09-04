8a6691c9706e6f14e55bf9f4a867bf8ca5914531
Roman Zipp <ich@ich.wtf>
2023-09-04 12:38:15 +0200

    Fix retry tests

--- tests/Support/MonitoredFailingJob.php
+++ tests/Support/MonitoredFailingJob.php
@@ -8,8 +8,14 @@ class MonitoredFailingJob extends BaseJob
 {
     use IsMonitored;
 
+    public static int $count = 0;
+
     public function handle(): void
     {
-        throw new IntentionallyFailedException('Whoops');
+        ++self::$count;
+
+        if (1 === self::$count) {
+            throw new IntentionallyFailedException('Whoops');
+        }
     }
 }
