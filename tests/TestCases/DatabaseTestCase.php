56e777bc615f32c191a74d2b45751947d87796c4
Roman Zipp <ich@ich.wtf>
2023-09-04 12:38:22 +0200

    Prepare database tests for sqlite

--- tests/TestCases/DatabaseTestCase.php
+++ tests/TestCases/DatabaseTestCase.php
@@ -8,37 +8,60 @@ use Illuminate\Support\Facades\Schema;
 
 abstract class DatabaseTestCase extends TestCase
 {
-    use RefreshDatabase;
-
-    public function refreshInMemoryDatabase()
-    {
-        throw new \RuntimeException('Only supporting MySQL');
+    use RefreshDatabase {
+        refreshTestDatabase as baseRefreshTestDatabase;
+        refreshInMemoryDatabase as baseRefreshInMemoryDatabase;
     }
 
-    protected function afterRefreshingDatabase()
+    protected function refreshInMemoryDatabase(): void
+    {
+        $this->createJobsTable();
+        $this->createFailedJobsTable();
+
+        $this->baseRefreshInMemoryDatabase();
+    }
+
+    protected function refreshTestDatabase(): void
+    {
+        $this->tryCreateJobsTables();
+
+        $this->baseRefreshTestDatabase();
+    }
+
+    protected function tryCreateJobsTables():void
     {
         if ( ! $this->app['db']->connection()->getSchemaBuilder()->hasTable('jobs')) {
-            Schema::create('jobs', function (Blueprint $table) {
-                $table->bigIncrements('id');
-                $table->string('queue')->index();
-                $table->longText('payload');
-                $table->unsignedTinyInteger('attempts');
-                $table->unsignedInteger('reserved_at')->nullable();
-                $table->unsignedInteger('available_at');
-                $table->unsignedInteger('created_at');
-            });
+            $this->createJobsTable();
         }
 
         if ( ! $this->app['db']->connection()->getSchemaBuilder()->hasTable('failed_jobs')) {
-            Schema::create('failed_jobs', function (Blueprint $table) {
-                $table->id();
-                $table->string('uuid')->unique();
-                $table->text('connection');
-                $table->text('queue');
-                $table->longText('payload');
-                $table->longText('exception');
-                $table->timestamp('failed_at')->useCurrent();
-            });
+            $this->createFailedJobsTable();
         }
+    }
+
+    private function createJobsTable(): void
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('queue')->index();
+            $table->longText('payload');
+            $table->unsignedTinyInteger('attempts');
+            $table->unsignedInteger('reserved_at')->nullable();
+            $table->unsignedInteger('available_at');
+            $table->unsignedInteger('created_at');
+        });
+    }
+
+    private function createFailedJobsTable(): void
+    {
+        Schema::create('failed_jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid')->unique();
+            $table->text('connection');
+            $table->text('queue');
+            $table->longText('payload');
+            $table->longText('exception');
+            $table->timestamp('failed_at')->useCurrent();
+        });
     }
 }
